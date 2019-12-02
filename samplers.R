315e6b52ac3ec7878451f216371358c12707f383
cjuracek <therealjuracek@gmail.com>
2019-12-02 14:16:32 -0500

    Small tweaks to function arguments + data structure of thetas.

--- samplers.R
+++ samplers.R
@@ -1,30 +1,35 @@
-gibbs_mvn <- function(Y, p = ncol(Y), n = nrow(Y), num_iter = 10000,
-                       nu_0 = 4, S_0 = cov(Y), Lambda_0 = cov(Y),
-                       mu_0 = matrix(apply(Y, 2, mean), p, 1)) {
+# Gibbs sampler for MCMC approximation of mean, covariance of multivariate normal
+# Based off of algorithm in Hoff, page 112
+#   nu_0, S_0: hyperparameters for covariance
+#   Lambda_0, mu_0: hyperparameters for mean
+# Returns
+gibbs_mvn <- function(Y, num_iter = 10000,
+                      nu_0 = 4, S_0 = cov(Y),
+                      Lambda_0 = cov(Y), mu_0 = matrix(apply(Y, 2, mean), ncol(Y), 1)) {
+  
+  p <- ncol(Y)
+  n <- nrow(Y)
   
   # https://stackoverflow.com/questions/19340079/how-to-declare-list-object-with-m-elements
-  thetas <- rep(list(matrix(NA, p, 1)), num_iter)
+  thetas <- matrix(NA, num_iter, p)
   sigmas <- rep(list(matrix(NA, p, p)), num_iter)
   y_bar <- matrix(apply(Y, 2, mean), p, 1)
   
-  for(i in 1:num_iter) {
+  # Initial 
+  thetas[1,] <- mu_0
+  for(i in seq_len(num_iter - 1)) {
     
-    # Full conditional for Theta
-    if(i == 1) {  # Generate intial Theta_0
-      thetas[[i]] <- mu_0
-    }
-    else{
-      mu_n <- compute_mu_n(Lambda_0, n, sigmas[[i - 1]], mu_0, y_bar)
-      Lambda_n <- compute_lambda_n(Lambda_0, n, sigmas[[i - 1]])
-      thetas[[i]] <- mvrnorm(1, mu_n, Lambda_n)
-    }
+    # Update theta
+    mu_n <- compute_mu_n(Lambda_0, n, sigmas[[i - 1]], mu_0, y_bar)
+    Lambda_n <- compute_lambda_n(Lambda_0, n, sigmas[[i - 1]])
+    thetas[i + 1,] <- mvrnorm(1, mu_n, Lambda_n)
     
     # Full conditional for Sigma
-    S_n <- compute_S_n(Y, thetas[[i]], S_0)
-    sigmas[[i]] <- solve(rWishart(1, nu_0 + n, solve(S_n))[,,1])
+    S_n <- compute_S_n(Y, thetas[i + 1,], S_0)
+    sigmas[[i + 1]] <- solve(rWishart(1, nu_0 + n, solve(S_n))[,,1])
     
   }
   
   #return(thetas)
-  return(c(thetas, sigmas))
+  return(list(theta = thetas, sigma = sigmas))
 }
