83ac8fdf8a359741de2a279b6f1093432396f279
cjuracek <therealjuracek@gmail.com>
2019-12-02 14:38:50 -0500

    Added usage file to give examples on how to use functions.

--- samplers.R
+++ samplers.R
@@ -1,8 +1,11 @@
+library(MASS)
+
 # Gibbs sampler for MCMC approximation of mean, covariance of multivariate normal
 # Based off of algorithm in Hoff, page 112
 #   nu_0, S_0: hyperparameters for covariance
 #   Lambda_0, mu_0: hyperparameters for mean
-# Returns
+# Returns named list of thetas (matrix w/each row a predictor mean) and
+#   sigmas (nested list of p x p covariance matrices)
 gibbs_mvn <- function(Y, num_iter = 10000,
                       nu_0 = 4, S_0 = cov(Y),
                       Lambda_0 = cov(Y), mu_0 = matrix(apply(Y, 2, mean), ncol(Y), 1)) {
@@ -19,17 +22,16 @@ gibbs_mvn <- function(Y, num_iter = 10000,
   thetas[1,] <- mu_0
   for(i in seq_len(num_iter - 1)) {
     
-    # Update theta
-    mu_n <- compute_mu_n(Lambda_0, n, sigmas[[i - 1]], mu_0, y_bar)
-    Lambda_n <- compute_lambda_n(Lambda_0, n, sigmas[[i - 1]])
-    thetas[i + 1,] <- mvrnorm(1, mu_n, Lambda_n)
-    
-    # Full conditional for Sigma
-    S_n <- compute_S_n(Y, thetas[i + 1,], S_0)
+    # Update Sigma via full conditional
+    S_n <- compute_S_n(Y, thetas[i,], S_0)
     sigmas[[i + 1]] <- solve(rWishart(1, nu_0 + n, solve(S_n))[,,1])
+    
+    # Update theta via full conditional
+    mu_n <- compute_mu_n(Lambda_0, n, sigmas[[i + 1]], mu_0, y_bar)
+    Lambda_n <- compute_lambda_n(Lambda_0, n, sigmas[[i + 1]])
+    thetas[i + 1,] <- mvrnorm(1, mu_n, Lambda_n)
     
   }
   
-  #return(thetas)
   return(list(theta = thetas, sigma = sigmas))
 }
